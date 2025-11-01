2240265324332db4e787ab9f8b9cb542dde14fed
manku888 <manishsingh.ms339@gmail.com>
2025-11-01 18:59:41 +0530

    auth reset

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'welcome to sindbad api';
+    return 'sidbad auth api is working';
   }
 }
